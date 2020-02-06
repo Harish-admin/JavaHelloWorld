1bb2cf7b58434a82fa40de428a8e9f6af15638c2
Harish <harish.k@medinovix.com>
2020-02-06 18:40:05 +0530

    adding program

---
+++ add.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20;
+		 int c=a+b;
+		 System.out.print("add is "+c);
+		
+
+	}
+
+}
